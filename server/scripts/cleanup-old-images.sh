4aa12e7f3d6162b84999611c710b5698de71bda3
Mohammad Shabeer <moshdev2213@gmail.com>
2025-11-24 19:21:14 +0530

    Remove: Unused History Images 02

--- server/scripts/cleanup-old-images.sh
+++ server/scripts/cleanup-old-images.sh
@@ -4,15 +4,16 @@ set -e
 # Config
 REGISTRY="mosh-do-docker-reg"         # DO registry namespace
 REPO="mailmate-server"                # repository name in that registry
+FULL_REPO="$REGISTRY/$REPO"           # full repository path
 
 # Fetch all tags
-TAGS=$(doctl registry repository list-tags $REPO --registry $REGISTRY --format Tag --no-header)
+TAGS=$(doctl registry repository list-tags $FULL_REPO --format Tag --no-header)
 
 # Delete all except prod and staging
 echo "$TAGS" | while read TAG; do
   if [[ "$TAG" != "production" && "$TAG" != "staging" ]]; then
     echo "Deleting old/unwanted image: $REPO:$TAG"
-    doctl registry repository delete $REPO --registry $REGISTRY --tag $TAG --force
+    doctl registry repository delete-tag $FULL_REPO $TAG --force
   else
     echo "Keeping image: $REPO:$TAG"
   fi
